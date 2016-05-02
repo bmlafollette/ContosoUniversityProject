16c53d554b3b48c1946127dc29d9f54bbe32a0f8
Bobbie LaFollette <colorwheelie@gmail.com>
2016-05-02 12:00:24 -0400

    Completed through first table in Instructors tab of webpage

---
+++ ContosoUniversityProject/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,16 @@
+﻿using ContosoUniversityProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversityProject.ViewModels
+{
+    public class InstructorIndexData
+    {
+        // Each holds the data for one of the tables on the Instructors page.
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
