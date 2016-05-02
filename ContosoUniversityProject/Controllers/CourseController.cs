36d4c0c5a8b6b8a3197ca48228ffa713e585b870
Bobbie LaFollette <colorwheelie@gmail.com>
2016-05-02 15:52:57 -0400

    Finished Tutorial - Advanced Entity Framework Scenarios

--- ContosoUniversityProject/Controllers/CourseController.cs
+++ ContosoUniversityProject/Controllers/CourseController.cs
@@ -17,10 +17,17 @@ namespace ContosoUniversityProject.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Course
-        public ActionResult Index()
+        public ActionResult Index(int? SelectedDepartment)
         {
-            // Use of word Include = eager loading.
-            var courses = db.Courses.Include(c => c.Department);
+            var departments = db.Departments.OrderBy(q => q.Name).ToList();
+            ViewBag.SelectedDepartment = new SelectList(departments, "DepartmentID", "Name", SelectedDepartment);
+            int departmentID = SelectedDepartment.GetValueOrDefault();
+
+            IQueryable<Course> courses = db.Courses
+                .Where(c => !SelectedDepartment.HasValue || c.DepartmentID == departmentID)
+                .OrderBy(d => d.CourseID)
+                .Include(d => d.Department);
+            var sql = courses.ToString();
             return View(courses.ToList());
         }
 
@@ -159,6 +166,21 @@ namespace ContosoUniversityProject.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult UpdateCourseCredits()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCourseCredits(int? multiplier)
+        {
+            if (multiplier != null)
+            {
+                ViewBag.RowsAffected = db.Database.ExecuteSqlCommand("UPDATE Course SET Credits = Credits * {0}", multiplier);
+            }
+            return View();
         }
     }
 }
