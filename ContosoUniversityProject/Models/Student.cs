2caf58717fc92484fad96cb7d4f3039599dab61a
Bobbie LaFollette <colorwheelie@gmail.com>
2016-05-02 10:00:31 -0400

    Completed Enable Migrations

--- ContosoUniversityProject/Models/Student.cs
+++ ContosoUniversityProject/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace ContosoUniversityProject.Models
     public class Student
     {
         public int ID { get; set; }
+        // The MaxLength attribute provides similar functionality to the StringLengt
+        // attribute but doesn't provide client side validation.
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannpt be longer that 50 characters.")]
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         // Below is a navigation property.
